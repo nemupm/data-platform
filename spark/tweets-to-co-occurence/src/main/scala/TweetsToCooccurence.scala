458e0b0453aeff70c0e52ce468c7b6accd41837b
nemupm <nemupm@gmail.com>
2020-08-10 21:11:24 +0900

    added test etl spark application to convert tweets into word co-occurrence

---
+++ spark/tweets-to-co-occurence/src/main/scala/TweetsToCooccurence.scala
@@ -0,0 +1,25 @@
+// scalastyle:off println
+package com.nemupm.spark
+
+import org.apache.spark.sql.SparkSession
+
+/** ETL job from tweets to co-occurrence of words */
+object TweetsToCooccurrence {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("Spark Pi")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    sc.hadoopConfiguration.set("fs.s3a.access.key", sys.env("S3_ACCESS_KEY"))
+    sc.hadoopConfiguration.set("fs.s3a.secret.key", sys.env("S3_SECRET_KEY"))
+    sc.hadoopConfiguration.set("fs.s3a.endpoint", sys.env("S3_ENDPOINT"))
+
+    val lines = sc.textFile("s3a://twitter/topics/twitter.sampled-stream/year=2020/month=08/day=10/twitter.sampled-stream+0+0000075019.json")
+    val lineLengths = lines.map(s => s.length)
+    val totalLength = lineLengths.reduce((a, b) => a + b)
+    println(s"total length is ${totalLength}")
+    spark.stop()
+  }
+}
+// scalastyle:on println
